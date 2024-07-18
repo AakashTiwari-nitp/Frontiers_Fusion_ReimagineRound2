2a0bf4d133c005366240fe46214d3ef54cc22cb9
AakashTiwari-nitp <aakasht.ug23.cs@nitp.ac.in>
2024-07-18 13:40:31 +0530

    file structure created and readme added

---
+++ README.md
@@ -0,0 +1,5 @@
+Contributors of this repo:-
+- Aman Mishra
+- Aakash Tiwari
+- Sandeep Kumar Gond
+- Ujjwal Kumar Choubey
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-500 text-3xl p-5'>Hii</h1>
+    </>
+  )
+}
+
+export default App
