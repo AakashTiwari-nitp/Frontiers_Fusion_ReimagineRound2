e75c1446b0e6242fc9264ba1993b38755931a9d7
AakashTiwari-nitp <aakasht.ug23.cs@nitp.ac.in>
2024-07-20 04:41:46 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Footer from "./Components/Footer";
 import HeroSection from "./Components/HeroSection";
 import PayBills from "./Components/PayBills";
 import RechargeSection from "./Components/RechargeSection";
+import Navbar from "./Components/Navbar/Navbar";
 import "./index.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <HeroSection />
       <PayBills />
       <RechargeSection />
