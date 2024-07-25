7dbe2bfd3e367069a491e1127a609d58515075f5
Aman Mishra <143611337+amannitp131@users.noreply.github.com>
2024-07-25 14:07:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect, Suspense } from 'react';
+
 import Footer from "./Components/Footer";
 import HeroSection from "./Components/HeroSection";
 import PayBills from "./Components/PayBills";
@@ -5,20 +7,42 @@ import "./index.css";
 import OfferSection from "./Components/OfferSection";
 import Acc from "./Components/Acc";
 import { lazy } from "react";
+import Preloader from './Components/Preloader';
 
 const Home = lazy(() => import("./Pages/Home"));
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating data fetching delay
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 4000)); 
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="bg-[#111] w-screen overflow-hidden">
-      <HeroSection />
-      <PayBills />
-      <OfferSection />
-      <Home />
-      <Acc />
-      <Footer />
+    <div>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <div className="bg-[#111] w-screen overflow-hidden">
+          <HeroSection />
+          <PayBills />
+          <OfferSection />  
+          
+          <Suspense fallback={<Preloader />}>
+            <Home />
+          </Suspense>
+          <Acc />
+          <Footer />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
